14c92ad3cc7f8c96e056f01c05a40aa372fcf67c
zzf1861 <zzf1861@qq.com>
2019-12-14 16:12:13 +0800

    first commit

---
+++ src/main/java/com/zzf/film/ZzfFilmApplication.java
@@ -0,0 +1,13 @@
+package com.zzf.film;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzfFilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZzfFilmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zzf/film/ZzfFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzf.film;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzfFilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
